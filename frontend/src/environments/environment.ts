f1365b3441275351ecc85abd1bfddf4b52e9783e
hectorww <125456147+hectorww@users.noreply.github.com>
2026-04-29 22:34:38 +0200

    Update environment configuration for production

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,3 +2,8 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8000',  // ← el puerto donde corre Symfony
 };
+
+export const environment = {
+  production: true,
+  apiUrl: 'agroconnect-tfg-production.up.railway.app',
+};
